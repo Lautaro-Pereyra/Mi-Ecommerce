45b081f22fc9f2649b16185821d5833b57b81df4
Lautaro-Pereyra <taro.lp27@gmail.com>
2023-08-31 12:50:09 -0300

    Update app web

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categoria,Producto
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','fecha_de_registro')
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','precio','categoria')
+    list_editable =('precio',)
